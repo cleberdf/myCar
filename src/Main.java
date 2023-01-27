cb7a3819d4d9f5262f7448a7a918930f19f302c7
bill <cleber.flauzino@hotmail.com>
2023-01-26 23:58:53 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//programa que faz a chamada do método fullThrottle
+public class Main {
+
+  public void fullThrottle() {
+    System.out.println("O carro está indo o mais rapido que pode! ");
+  }
+
+  public void speed(int maxSpeed) {
+    System.out.println("Velocidade máxima permitida é: " + maxSpeed);
+  }
+
+}
